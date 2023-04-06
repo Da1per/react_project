cbddf1275e2feadb823dab6bdee036c5fcc75985
Vadim Gritskevich <vadim.gritskevich33@gmail.com>
2023-04-06 20:40:37 +0300

    Edited Header.js, Header.css by VadimGric

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,22 @@ import './css/Header.css';
 function Header() {
   return (
     <div className="App">
+      <div className='UpPanel'>
+        <div className='ButtonAutorization'>
+          <button>
+            <p className = "SingUp">Sing up</p>
+          </button>
+          <button>
+            <p className='LogIn'>LogIn</p>
+          </button>
+        </div>
+        <div className='SearchString'>
+          <img src="images/logo.jpg" alt="BigCo Inc. logo"/>
+          
+        </div>
+      </div>
     </div>
+    
   );
 }
 
