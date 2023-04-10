3517ab6bd7e454f66a11a513d57faa1bf07f85f0
Da1per <kiborg7075@gmail.com>
2023-04-10 13:11:08 +0300

    edit BodyH and LeftBar by DaLPer

--- src/BodyH.js
+++ src/BodyH.js
@@ -1,12 +1,16 @@
 import './css/BodyH.css';
 import PlayList from './PlayList';
 import MusicPlayer from './MusicPlayer';
+import musicPad from './images/music_pad.png'
 function BodyH() {
     return (
       <div className="body_h">
+          <div className='body_h_header'>
+            <h1>Your music is here</h1>
+            <img className='body_h_img' src={musicPad} alt='music img' ></img>
+            </div>
           <PlayList/>
           <MusicPlayer/>
-        test text
       </div>
     );
   }
