a3be9fee0bd4246aad6b9b4e6ff050d1e266575a
Da1per <kiborg7075@gmail.com>
2023-04-12 14:56:08 +0300

    edit files by DaLPer

--- src/BodyH.js
+++ src/BodyH.js
@@ -8,8 +8,9 @@ function BodyH() {
           <div className='body_h_header'>
             <h1>Your music is here</h1>
             <img className='body_h_img' src={musicPad} alt='music img' ></img>
-            </div>
-          <PlayList/>
+          </div>
+          <PlayList name={'NOW LISTENING'} num={12}/>
+          <PlayList name={'DESERT PLAYLISTS'} num={12}/>
           <MusicPlayer/>
       </div>
     );
--- src/PlayList.js
+++ src/PlayList.js
@@ -1,10 +1,28 @@
 import './css/PlayList.css';
-function PlayList() {
-    return (
-      <div className="playList">
-
+function PlayList(props) {
+  let el=[]
+  for(let i=0;i<props.num;i++){
+    el.push(
+    <div className='playlist_list_block'> 
+    </div>)
+  }
+  return (
+    <div className="playList">
+      <div className='playlist_header'>
+        <div className='playlist_header_block'>
+          <h2 className='playlist_header_h1'>{props.name}</h2>
+          <div className='playlist_header_line'></div>
+        </div>
+        <div className='playlist_header_expand'>
+          <div className='link'>expand</div>
+        </div>
       </div>
-    );
+      <div className='playlist_list'>
+        {el}
+      
+      </div>
+    </div>
+  );
   }
   
   export default PlayList;
