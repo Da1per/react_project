f8af8ebd5f2ff00eaaeaa7718c3279e047309d04
Vadim Gritskevich <vadim.gritskevich33@gmail.com>
2023-04-13 20:37:08 +0300

    change MusicPlayer.js MusicPlayer.css by VadimGric

--- src/MusicPlayer.js
+++ src/MusicPlayer.js
@@ -4,6 +4,24 @@ class MusicPlayer extends React.Component {
   render(){
     return (
       <div className="App">
+        <div className='container'>
+          <div className='player'>
+           
+            
+              <div className='controls'>
+                <div><i class="fa-solid fa-backward fa-xl"></i></div>
+                <div><i class="fa-solid fa-play fa-beat-fade fa-2xl"></i></div>
+                <div><i class="fa-solid fa-forward fa-xl"></i></div>
+
+              </div>
+              <input type='range'  value='0' id='progress'></input>
+              <div className='img_track'></div>
+
+            
+              
+            
+          </div>
+        </div>
 
       </div>
     );
