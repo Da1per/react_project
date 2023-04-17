024f44834b424a845c946b60b60a1a45b1e909e2
Da1per <kiborg7075@gmail.com>
2023-04-17 21:08:03 +0300

    edit by DaLPer

--- src/MusicPlayer.js
+++ src/MusicPlayer.js
@@ -1,30 +1,119 @@
 import './css/MusicPlayer.css';
-import React from 'react';
-class MusicPlayer extends React.Component {
-  render(){
-    return (
-      <div className="App">
-        <div className='container'>
-          <div className='player'>
-           
-            
-              <div className='controls'>
-                <div><i class="fa-solid fa-backward fa-xl"></i></div>
-                <div><i class="fa-solid fa-play fa-beat-fade fa-2xl"></i></div>
-                <div><i class="fa-solid fa-forward fa-xl"></i></div>
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import qala from "./music/sound1.wav";
+import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
+import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
+import { IconContext } from "react-icons";
 
-              </div>
-              <input type='range'  value='0' id='progress'></input>
-              <div className='img_track'></div>
+function MusicPlayer() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [time, setTime] = useState({
+    min: "",
+    sec: ""
+  });
+  const [currTime, setCurrTime] = useState({
+    min: "",
+    sec: ""
+  });
 
-            
-              
-            
-          </div>
-        </div>
+  const [seconds, setSeconds] = useState();
 
+  const [play, { pause, duration, sound }] = useSound(qala);
+
+  useEffect(() => {
+    if (duration) {
+      const sec = duration / 1000;
+      const min = Math.floor(sec / 60);
+      const secRemain = Math.floor(sec % 60);
+      setTime({
+        min: min,
+        sec: secRemain
+      });
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sound) {
+        setSeconds(sound.seek([]));
+        const min = Math.floor(sound.seek([]) / 60);
+        const sec = Math.floor(sound.seek([]) % 60);
+        setCurrTime({
+          min,
+          sec
+        });
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [sound]);
+
+  const playingButton = () => {
+    if (isPlaying) {
+      pause();
+      setIsPlaying(false);
+    } else {
+      play();
+      setIsPlaying(true);
+    }
+  };
+
+  return (
+    <div className="component">
+      <h2>Playing Now</h2>
+      <img className="musicCover" src="" />
+      <div>
+        <h3 className="title">Drum&boost</h3>
+        <p className="subTitle">orex47</p>
       </div>
-    );
-  }
+      <div>
+        <div className="time">
+          <p>
+            {currTime.min}:{currTime.sec}
+          </p>
+          <p>
+            {time.min}:{time.sec}
+          </p>
+        </div>
+        <input
+          type="range"
+          min="0"
+          max={duration / 1000}
+          default="0"
+          value={seconds}
+          className="timeline"
+          onChange={(e) => {
+            sound.seek([e.target.value]);
+          }}
+        />
+      </div>
+      <div>
+        <button className="playButton">
+          <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+            <BiSkipPrevious />
+          </IconContext.Provider>
+        </button>
+        {!isPlaying ? (
+          <button className="playButton" onClick={playingButton}>
+            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+              <AiFillPlayCircle />
+            </IconContext.Provider>
+          </button>
+        ) : (
+          <button className="playButton" onClick={playingButton}>
+            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+              <AiFillPauseCircle />
+            </IconContext.Provider>
+          </button>
+        )}
+        <button className="playButton">
+          <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+            <BiSkipNext />
+          </IconContext.Provider>
+        </button>
+      </div>
+    </div>
+  );
 }
+
   export default MusicPlayer;
