6a69425b9919803a12110b67af3485a4a9f70a29
Da1per <kiborg7075@gmail.com>
2023-04-19 13:05:33 +0300

    edit by DaLPer

--- src/LeftBar.js
+++ src/LeftBar.js
@@ -8,7 +8,7 @@ const LeftBar =({b1,b2,b3,b4,b5,b6})=> {
           <img className='logo_img' src={logo_site} alt='logo'></img>
           <p className='logo_name'>desert sound</p>
         </a>
-        <ul>
+        <ul className='left_bar_links'>
           <li><button onClick={b1} className='link' href='#'>GENRES</button></li>
           <li><button onClick={b2} className='link' href='#'>NEW TRACKS</button></li>
           <li><button onClick={b3} className='link' href='#'>TOP TRACKS</button></li>
