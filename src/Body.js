1be950e270a497f11a8151a0fd7bd8b01c339035
Da1per <kiborg7075@gmail.com>
2023-04-22 13:23:24 +0300

    edit body.js and leftbar.js by DaLPer

--- src/Body.js
+++ src/Body.js
@@ -8,75 +8,33 @@ import Top_tracks from './Top_tracks'
 import New_tracks from './New_tracks'
 import Podcast from './Podcast'
 import MusicPlayer from './MusicPlayer';
-class Body extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      stat:'BodyH'
+import { useEffect, useState } from "react";
+function Body() {
+  const [activeTabs, setActiveTabs] = useState({
+    tab:"BodyH",
+  });
+let stat2=''
+  const activeBbut=(stat)=>{
+    stat2=stat
+    setActiveTabs({tab:stat2})
+    console.log(stat2)
+  }
+  useEffect(() => {
+    if(stat2){
+      setActiveTabs({tab:stat2})
     }
-    this.activeTabs = this.activeTabs.bind(this);
-    this.activeTabs1 = this.activeTabs1.bind(this);
-    this.activeTabs2 = this.activeTabs2.bind(this);
-    this.activeTabs3 = this.activeTabs3.bind(this);
-    this.activeTabs4 = this.activeTabs4.bind(this);
-    this.activeTabs5 = this.activeTabs5.bind(this);
-  }  
-  activeTabs(){
-    this.setState(prevState => {
-      return {
-        stat:'Generes'
-      }
-    })
-  }
-  activeTabs1(){
-    this.setState(prevState => {
-      return {
-        stat:'New_artist'
-      }
-    })
-  }
-  activeTabs2(){
-    this.setState(prevState => {
-      return {
-        stat:'Top_tracks'
-      }
-    })
-  }
-  activeTabs3(){
-    this.setState(prevState => {
-      return {
-        stat:'New_tracks'
-      }
-    })
-    }  
-  activeTabs4(){
-    this.setState(prevState => {
-      return {
-        stat:'Podcast'
-      }
-    })
-  }
-  activeTabs5(){
-    this.setState(prevState => {
-      return {
-        stat:'BodyH'
-      }
-    })
-  }
-  render(){
-    return (
-      <div className="main_body">
-        <LeftBar b1={this.activeTabs}b2={this.activeTabs1}b3={this.activeTabs2}b4={this.activeTabs3}b5={this.activeTabs4}b6={this.activeTabs5}/>
-        {(this.state.stat=='BodyH')?<BodyH/>:null}
-        {(this.state.stat=='Generes')?<Generes/>:null}
-        {(this.state.stat=='New_artist')?<New_artist/>:null}
-        {(this.state.stat=='Top_tracks')?<Top_tracks/>:null}
-        {(this.state.stat=='Podcast')?<Podcast/>:null}
-        {(this.state.stat=='New_tracks')?<New_tracks/>:null}
-        <MusicPlayer/>
-      </div>
-    );
+  })
+  return (
+    <div className="main_body">
+      <LeftBar  setActive={activeBbut}/>
+      {(activeTabs.tab=="BodyH")?<BodyH/>:null}
+      {(activeTabs.tab=="Generes")?<Generes/>:null}
+      {(activeTabs.tab=="New_artist")?<New_artist/>:null}
+      {(activeTabs.tab=="Top_tracks")?<Top_tracks/>:null}
+      {(activeTabs.tab=="Podcast")?<Podcast/>:null}
+      {(activeTabs.tab=="New_tracks")?<New_tracks/>:null}
+      <MusicPlayer/>
+    </div>
+  );
 }
-}
-
 export default Body;
--- src/LeftBar.js
+++ src/LeftBar.js
@@ -1,23 +1,39 @@
 import './css/LeftBar.css';
 import logo_site from'./images/logo.svg';
 import React from 'react';
-const LeftBar =({b1,b2,b3,b4,b5,b6})=> {
+const LeftBar =({setActive})=> {
+  const active1=()=>{
+    setActive('BodyH')
+  }
+  const active2=()=>{
+    setActive('Generes')
+  }
+  const active3=()=>{
+    setActive('New_artist')
+  }
+  const active4=()=>{
+    setActive('Top_tracks')
+  }
+  const active5=()=>{
+    setActive('Podcast')
+  }
+  const active6=()=>{
+    setActive('New_tracks')
+  }
     return (
       <div className="left_bar">
-        <a className='logo' href='#' onClick={b6} >
+        <a className='logo' href='#' onClick={active1} >
           <img className='logo_img' src={logo_site} alt='logo'></img>
           <p className='logo_name'>desert sound</p>
         </a>
         <ul className='left_bar_links'>
-          <li><button onClick={b1} className='link' href='#'>GENRES</button></li>
-          <li><button onClick={b2} className='link' href='#'>NEW TRACKS</button></li>
-          <li><button onClick={b3} className='link' href='#'>TOP TRACKS</button></li>
-          <li><button onClick={b4} className='link' href='#'>TOP ARTIST</button></li>
-          <li><button onClick={b5} className='link ' href='#'>PODCAST</button></li>
+          <li><button onClick={active2} className='link' href='#'>GENRES</button></li>
+          <li><button onClick={active6} className='link' href='#'>NEW TRACKS</button></li>
+          <li><button onClick={active4} className='link' href='#'>TOP TRACKS</button></li>
+          <li><button onClick={active3} className='link' href='#'>TOP ARTIST</button></li>
+          <li><button onClick={active5} className='link ' href='#'>PODCAST</button></li>
         </ul>
       </div>
     );
 }
-
-  
   export default LeftBar;
