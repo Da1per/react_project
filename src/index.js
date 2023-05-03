f19a2390cbb8b05238cb48d4a8992aa1f4054e7d
Da1per <kiborg7075@gmail.com>
2023-05-03 18:19:02 +0300

    add reduce DaLPer

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.sass';
 import Main from './Main';
-
+import { Provider } from 'react-redux'
+import store from './store/reducers/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Main />
+    <Provider store={store}>
+      <Main />
+    </Provider>
   </React.StrictMode>
 );
 
