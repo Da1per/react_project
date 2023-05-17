d19bd94ae709549aa43c65108d46ebf5c2d92d66
Da1per <kiborg7075@gmail.com>
2023-05-17 20:06:03 +0300

    fix bugs by DaLPer

--- src/js/components/AuthMod.js
+++ src/js/components/AuthMod.js
@@ -8,7 +8,9 @@ const AuthMod =({active,but})=> {
                         <label className='pop_up_input_label' for="login">Login</label>
                         <input className='pop_up_input_text' placeholder="lol" type="" name='login'></input>
                         <label className='pop_up_input_label' for="login">Password</label>
+
                         <input className='pop_up_input_text' placeholder="123456" type="" name='password'></input>
+                        
                         <input className='pop_up_input_button' type="button" value='login'></input>
                     </form>
                 </div>
